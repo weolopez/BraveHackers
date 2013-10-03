91e6d4b45d19ac3794cc57649b3b8af772bcad7b
kelleymantione <kelleymantione@mac.com>
2013-10-03 08:46:52 -0400

    removing course service

--- src/main/java/model/AccessManager.java
+++ src/main/java/model/AccessManager.java
@@ -8,20 +8,12 @@ import java.util.List;
 import dao.Access;
 import dao.Database;
 import dto.Buyer;
-import dto.Course;
 import dto.Line;
 import dto.Product;
 
 
 
 public class AccessManager {
-	public ArrayList<Course> getCourses() throws Exception {
-		ArrayList<Course> courseList = new ArrayList<Course>();
-		Access access = new Access();
-		courseList = access.getCourses(getConnection());
-		return courseList;
-	}
-	
 	
 	public ArrayList<Line> getLines() throws Exception {
 		ArrayList<Line> lineList = new ArrayList<Line>();
