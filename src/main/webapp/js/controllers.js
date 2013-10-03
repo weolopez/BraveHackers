76ee669bdf1d50f25110f07585e023e422ad6413
ML5174 <ML5174@GACDTL03ML5174.ITServices.sbc.com>
2013-10-02 21:49:42 -0400

    Merge origin/master
    Conflicts:
    	BraveHackers/src/main/webapp/index.jsp

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -2,16 +2,75 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
+angular.module('myApp.controllers', ['AngularGM', 'ngTouch']).
         controller('MyCtrl1', function() {
-})
+        })
         .controller('MyCtrl2', function() {
-})
+        })
         .controller('MyCtrl3', function() {
-})
+        })
         .controller('MyCtrl4', function() {
-})
-        .controller('GeocodeCtrl', function() {
-})
+        })
+        .controller('GeomapCtrl', function($scope) {
+            var overlay;
+            
+            $scope.dragObj = {
+                helper: 'clone',
+                stop: function(e) {
+                    var point = new google.maps.Point(e.pageX,e.pageY);
+                    var ll = overlay.getProjection().fromContainerPixelToLatLng(point);
+                }
+            }
+            var x = document.getElementById("demo");
+
+            $scope.lat = 46;
+            $scope.lng = -122;
+
+            function showPosition(position)
+            {
+                $scope.lat = position.coords.latitude;
+                $scope.lng = position.coords.longitude;
+                $scope.center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                $scope.$apply();
+            }
+            ;
+
+
+
+            if (navigator.geolocation)
+            {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            }
+            else {
+                x.innerHTML = "Geolocation is not supported by this browser.";
+            }
+
+
+            $scope.houses = [{
+                    name: 'myHouse',
+                    lat: 46,
+                    lng: -122
+                }];
+            $scope.options = {
+                map: {
+                    center: new google.maps.LatLng($scope.lat, $scope.lng),
+                    zoom: 21,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                },
+                marker: {
+                    clickable: false,
+                    draggable: true
+                }
+            };
+            $scope.setHouseLocation = function(house, marker) {
+                var position = marker.getPosition();
+                house.lat = position.lat();
+                house.lng = position.lng();
+            };
+            $scope.clickDrag = function(e) {
+                var a = e;
+            }
+
+        })
         ;
         
