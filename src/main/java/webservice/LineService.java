90367afae09f33f41533ad1c779ce57439f583b2
kelleymantione <kelleymantione@mac.com>
2013-10-03 08:28:20 -0400

    added error handling, created user service

--- src/main/java/webservice/LineService.java
+++ src/main/java/webservice/LineService.java
@@ -6,67 +6,14 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 
 import model.AccessManager;
-import model.UserManager;
-import dto.Acknowledgement;
 import dto.Line;
-import dto.User;
 
 @Path("/lineService")
 public class LineService {
-	@GET
-	@Path("/users")
-	@Produces("application/json")
-	public ArrayList<User> users() {
-		String users = null;
-		ArrayList<User> userList = new ArrayList<User>();
-		try {
-			userList = new UserManager().getUsers();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return userList;
-	}
-	
-	@PUT
-	@Produces(MediaType.TEXT_PLAIN)
-	@Path("/adduser")
-	public String adduser( @FormParam("firstname" ) String firstname , @FormParam("lastname" ) String lastname,
-			@FormParam("password" ) String password ,
-			@FormParam("username" ) String username ,
-			@FormParam("authmethod" ) String authmethod ,
-			@FormParam("lat" ) String lat ,
-			@FormParam("lng" ) String lng 
-		) {
-		
-		User user = new User();
-		user.setFirstname(firstname);
-		user.setLastname(lastname);
-		user.setPassword(password);
-		user.setUsername(username);
-		user.setAuthmethod(authmethod);
-		if (lat!=null && lat.trim().length() !=0)
-		{
-			user.setLat(Double.parseDouble(lat));
-		}
-		if (lng!=null && lng.trim().length() !=0)
-		{
-			user.setLng(Double.parseDouble(lng));
-		}
-		int id =0;
-		try {
-			id = new UserManager().addUser(user);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return String.valueOf(id);
-		
-	}
 	
 	@GET
 	@Path("/getlines")
@@ -80,40 +27,6 @@ public class LineService {
 			e.printStackTrace();
 		}
 		return lineList;
-	}
-	
-	@PUT
-	@Produces("application/json")
-	@Path("/updatelocation")
-	public Acknowledgement updateLocation( @FormParam("lat" ) String inlat , @FormParam("lng" ) String inlng,
-			@FormParam("id" ) String id 
-		) {
-		Acknowledgement ack = new Acknowledgement();
-		ack.setSuccess(false);
-		double lat = 0;
-		double lng = 0;
-		int userid = -1;
-				
-		if (inlat!=null && inlat.trim().length() !=0)
-		{
-			lat = Double.parseDouble(inlat);
-		}
-		if (inlng!=null && inlng.trim().length() !=0)
-		{
-			lng = Double.parseDouble(inlng);
-		}
-		if (id!=null && id.trim().length() !=0)
-		{
-			userid = Integer.parseInt(id);
-		}
-		try {
-			new UserManager().updateLocation(lat,lng,userid);
-			ack.setSuccess(true);
-		} catch (Exception e) {
-			ack.setReason(e.getMessage());
-		}
-		return ack;
-		
 	}
 	
 	
@@ -139,9 +52,7 @@ public class LineService {
 		 line.setType(type);
 		 line.setCount(count);
 		 line.setVote(vote);
-		 
-	
-		 
+		 	 
 		 
 		try {
 			lineId = new AccessManager().addLine(line);
@@ -151,6 +62,8 @@ public class LineService {
 		return lineId;
 		 
 	}
+	
+	
 	
 	
 }
