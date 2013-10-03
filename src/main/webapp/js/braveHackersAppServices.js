bf42afd5949db9661b063ac1ca1469c995cec276
weolopez <weolopez@yahoo.com>
2013-10-03 11:55:53 -0400

    added new braveHackers project

--- src/main/webapp/js/braveHackersAppServices.js
+++ src/main/webapp/js/braveHackersAppServices.js
@@ -5,6 +5,15 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('braveHackers.services', [])
+angular.module('braveHackers.services', ['ngResource'])
         .value('version', '0.2')
+        .factory('Line', function($resource) {
+            return $resource('/BraveHackers/crowds/lineService/addline',
+                    {alt: 'json'},
+            {
+                get: {method: 'JSONP', params: {visibility: '@self'}}, 
+                replies: {method: 'JSONP', params: {visibility: '@self', comments: '@comments'}}
+            }
+            );
+        })
         ;
